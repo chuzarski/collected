d7780c36693c931e77b9f3b6364e02caec6d83ff
Cody Huzarski <chuzarski@gmail.com>
2021-06-04 19:25:16 -0400

    Initial commit

---
+++ src/test/java/csi3370/team2/CollectedTest.java
@@ -0,0 +1,21 @@
+package csi3370.team2;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class CollectedTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
